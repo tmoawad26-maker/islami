618405882b01a742d6ab1157259a2147cdf80126
sh2 <tmoawad26@gamil.com>
2026-07-14 15:41:33 +0300

    inital project

---
+++ lib/islami_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami/core/utils/app_routes.dart';
+import 'package:islami/screens/main_layout.dart';
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'Islami App',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        AppRoutes.mainLayoutRoute : (_) => MainLayout(),
+      },
+      initialRoute: AppRoutes.mainLayoutRoute,
+    );
+  }
+}
