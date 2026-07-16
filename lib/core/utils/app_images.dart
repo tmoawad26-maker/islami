796ba3db9bd4b9f4f195b49c5446a5a0c1ecf181
sh2 <tmoawad26@gamil.com>
2026-07-16 14:41:53 +0300

    adding app_images contains image paths

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for iconsHeadith
+  /// assets/icons/headith.png
+  static const String iconsHeadith = "assets/icons/headith.png";
+
+  /// Assets for iconsIcRadio
+  /// assets/icons/ic_radio.png
+  static const String iconsIcRadio = "assets/icons/ic_radio.png";
+
+  /// Assets for iconsIcSebha
+  /// assets/icons/ic_sebha.png
+  static const String iconsIcSebha = "assets/icons/ic_sebha.png";
+
+  /// Assets for iconsIcTime
+  /// assets/icons/ic_time.png
+  static const String iconsIcTime = "assets/icons/ic_time.png";
+
+  /// Assets for iconsNecklaceIslamSvgrepoCom 1
+  /// assets/icons/necklace-islam-svgrepo-com 1.png
+  static const String iconsNecklaceIslamSvgrepoCom1 =
+      "assets/icons/necklace-islam-svgrepo-com 1.png";
+
+  /// Assets for iconsQuranIc
+  /// assets/icons/quran_ic.png
+  static const String iconsQuranIc = "assets/icons/quran_ic.png";
+
+  /// Assets for iconsRadio
+  /// assets/icons/radio.png
+  static const String iconsRadio = "assets/icons/radio.png";
+
+  /// Assets for iconsTime
+  /// assets/icons/time.png
+  static const String iconsTime = "assets/icons/time.png";
+
+  /// Assets for images2
+  /// assets/images/2.0x
+  static const String images2 = "assets/images/2.0x";
+
+  /// Assets for images20xBackHomeImage2x
+  /// assets/images/2.0x/back_home_image@2x.png
+  static const String images20xBackHomeImage2x =
+      "assets/images/2.0x/back_home_image@2x.png";
+
+  /// Assets for images3
+  /// assets/images/3.0x
+  static const String images3 = "assets/images/3.0x";
+
+  /// Assets for images30xBackHomeImage3x
+  /// assets/images/3.0x/back_home_image@3x.png
+  static const String images30xBackHomeImage3x =
+      "assets/images/3.0x/back_home_image@3x.png";
+
+  /// Assets for imagesBackHomeImage
+  /// assets/images/back_home_image.png
+  static const String imagesBackHomeImage = "assets/images/back_home_image.png";
+
+  /// Assets for imagesBackgrondImage
+  /// assets/images/backgrond_image.png
+  static const String imagesBackgrondImage =
+      "assets/images/backgrond_image.png";
+
+  /// Assets for imagesBranding
+  /// assets/images/branding.png
+  static const String imagesBranding = "assets/images/branding.png";
+
+  /// Assets for imagesSplashIcon2
+  /// assets/images/splash_icon_2.png
+  static const String imagesSplashIcon2 = "assets/images/splash_icon_2.png";
+}
